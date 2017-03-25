642f9fdf145c1ba3334bee76a47244cde17a7768
Arctic Ice Studio <development@arcticicestudio.com>
2017-03-25 17:59:23 +0100

    GHI-#12 Implement support for the "jdee" package

--- nord-theme.el
+++ nord-theme.el
@@ -270,6 +270,25 @@
     `(erm-syn-errline ((,class (:foreground ,nord11 :underline t))))
     `(erm-syn-warnline ((,class (:foreground ,nord13 :underline t))))
 
+    ;; > Java Development Environment for Emacs
+    `(jdee-db-active-breakpoint-face ((,class (:background ,nord2 :weight bold))))
+    `(jdee-bug-breakpoint-cursor ((,class (:background ,nord2))))
+    `(jdee-db-requested-breakpoint-face ((,class (:foreground ,nord13 :background ,nord2 :weight bold))))
+    `(jdee-db-spec-breakpoint-face ((,class (:foreground ,nord14 :background ,nord2 :weight bold))))
+    `(jdee-font-lock-api-face ((,class (:foreground ,nord4))))
+    `(jdee-font-lock-code-face ((,class (:slant italic))))
+    `(jdee-font-lock-constant-face ((,class (:foreground ,nord-keyword))))
+    `(jdee-font-lock-constructor-face ((,class (:foreground ,nord-method))))
+    `(jdee-font-lock-doc-tag-face ((,class (:foreground ,nord7))))
+    `(jdee-font-lock-link-face ((,class (:underline t))))
+    `(jdee-font-lock-modifier-face ((,class (:foreground ,nord-keyword))))
+    `(jdee-font-lock-number-face ((,class (:foreground ,nord-numeric))))
+    `(jdee-font-lock-operator-fac ((,class (:foreground ,nord-operator))))
+    `(jdee-font-lock-package-face ((,class (:foreground ,nord-class))))
+    `(jdee-font-lock-pre-face ((,class (:foreground ,nord-comment :slant italic))))
+    `(jdee-font-lock-private-face ((,class (:foreground ,nord-keyword))))
+    `(jdee-font-lock-public-face ((,class (:foreground ,nord-keyword))))
+    `(jdee-font-lock-variable-face ((,class (:foreground ,nord-variable))))
     ;; +--- UI ---+
 ))
 
