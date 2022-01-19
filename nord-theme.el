41a45e857165e1441e6e1df7bbc3d607e3f340fd
Pedro A. Aranda Gutierrez <paaguti@gmail.com>
2022-01-19 11:24:42 +0100

    Better define the foreground colour

--- nord-theme.el
+++ nord-theme.el
@@ -244,7 +244,7 @@
     `(minibuffer-prompt ((,class (:foreground ,nord8 :weight bold))))
     `(mm-command-output ((,class (:foreground ,nord8))))
     `(mode-line ((,class (:foreground ,nord8 :background ,nord3))))
-    `(mode-line-buffer-id ((,class (:inherit mode-line :weight bold))))
+    `(mode-line-buffer-id ((,class (:foreground ,nord8 :weight bold))))
     `(mode-line-highlight ((,class (:inherit highlight))))
     `(mode-line-inactive ((,class (:foreground ,nord4 :background ,nord-uniform-mode-lines-background))))
     `(next-error ((,class (:inherit error))))
